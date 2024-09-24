d29dd700c1d5dc3cca9895ebc5662b2d25ddec26
itkomuna <itkomunanovisad@gmail.com>
2024-09-24 21:50:07 +0200

    prva jedinica gotova komantari i promenljive

--- 0-komentari-promenljive.py
+++ 0-komentari-promenljive.py
@@ -1,1 +1,49 @@
-print('Krecemo sa komentarima i promenljivama')
+# komentari i opisju delove koda i sprecavaju neke delove da se ne izvrsavaju
+# print('Krecemo sa komentarima i promenljivama')
+
+#################################
+##  Varijable / Promeljive     ##
+#################################
+naslov = 'Promenljive'
+print(naslov)
+ime = 'Freeda'
+godine = 4
+print("Moj psic se zove " + ime + " i ima " + str(godine) + ' godine')
+broj1 = 100
+broj2 = "200"
+print(broj1 + int(broj2))
+
+x = 100
+x = 'sloba'
+print(x)
+
+a, b, c = 30, 390, 3009
+print(c)
+
+x = y = z = 200
+print(x, y, z)
+
+ime, prezime, email = 'Sloba', 'Miric', 'slobamiric@gmial.com'
+print(ime, prezime, email)
+
+auto = 'Mercedes'
+AUTO = 'BMW'
+print(AUTO)
+
+#################################
+##  Dobra praksa imenovanja    ##
+#################################
+first_name = 'Sloba'
+firstName = 'Sloba'
+_first_name = 'Sloba'
+name_1 = 'Sloba'
+
+#################################
+##  Kako ne trreba imenovati varijable
+#################################
+
+# 1name = 'Sloba'
+# first-name = 'Sloba'
+# !name = 'Sloba'
+# first+name = 'Sloba'
+# first name = 'Sloba'
