390dd3dac8561363a1f4afd6c99301285be805eb
itkomuna <itkomunanovisad@gmail.com>
2024-10-08 21:37:54 +0200

    funkcije uvod

---
+++ 8-metode-ugradjene-funkcije.py
@@ -0,0 +1,11 @@
+def pozdrav(ime):
+    print(f"Zdravo, {ime}")
+
+pozdrav('Amele')
+pozdrav('Snezo')
+pozdrav('Branislave')
+pozdrav('Bojana')
+pozdrav('Marko')
+
+ime = input('Unesite svoje ime: ')
+print('Dobrodosli', ime)
